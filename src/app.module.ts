a3981e23747b1b4211070407e18508692203e510
Marco Silva <marcoantonio02016@outlook.com>
2025-09-14 11:00:17 -0300

    feat: add all modules to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
-import { ActivityBranchesModule } from './activity-branches/activity-branches.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CompaniesModule } from './companies/companies.module';
-import { PasswordRecoveryTokensModule } from './password-recovery-tokens/password-recovery-tokens.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { CompaniesModule } from './companies/companies.module';
 import { UsersModule } from './users/users.module';
+import { ActivityBranchesModule } from './activity-branches/activity-branches.module';
+import { ServicesModule } from './services/services.module';
+import { EmployeesModule } from './employees/employees.module';
+import { EmployeeServicePreferencesModule } from './employee-service-preferences/employee-service-preferences.module';
+import { ClientsModule } from './clients/clients.module';
+import { AppointmentsModule } from './appointments/appointments.module';
+import { AttendancesModule } from './attendances/attendances.module';
+import { AttendanceServicesModule } from './attendance-services/attendance-services.module';
+import { AttendanceEmployeesModule } from './attendance-employees/attendance-employees.module';
+import { PasswordRecoveryTokensModule } from './password-recovery-tokens/password-recovery-tokens.module';
+import { DefaultActivityServicesModule } from './default-activity-services/default-activity-services.module';
 
 @Module({
   imports: [
@@ -13,7 +22,16 @@ import { UsersModule } from './users/users.module';
     CompaniesModule,
     UsersModule,
     ActivityBranchesModule,
+    ServicesModule,
+    EmployeesModule,
+    EmployeeServicePreferencesModule,
+    ClientsModule,
+    AppointmentsModule,
+    AttendancesModule,
+    AttendanceServicesModule,
+    AttendanceEmployeesModule,
     PasswordRecoveryTokensModule,
+    DefaultActivityServicesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/default-activity-services/default-activity-services.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('default-activity-services')
+export class DefaultActivityServicesController {}
